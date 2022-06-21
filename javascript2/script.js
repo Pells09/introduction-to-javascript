9fe8de9843fa95bdb53eaa57dfe1630789a9c187
Pells09 <105182072+Pells09@users.noreply.github.com>
2022-06-21 17:44:30 +0100

    Add files via upload

---
+++ javascript2/script.js
@@ -0,0 +1,4 @@
+
+document.getElementById ("name"). innerHTML = "My name is Fagbenro Pelumi.";
+document.getElementById ("height"). innerHTML = "I am 5.8ft tall.";
+document.getElementById ("country"). innerHTML = "I a from Nigeria.";
