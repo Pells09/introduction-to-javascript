6dbffc3ee31e643fe8ab018d8515fb77b886a6db
Pells09 <105182072+Pells09@users.noreply.github.com>
2022-06-22 03:57:49 +0100

    Add files via upload

---
+++ javascript2/script.js
@@ -0,0 +1,9 @@
+var myName = "Fagbenro Habeeb Pelumi.";
+var height = 5.8 ;
+var country = "Nigeria." ;
+
+document.write (" My name is " + myName);
+document.write("<br>")
+document.write (" My height is " + height + " ft. ");
+document.write("<br>")
+document.write (" I am from " + country);
