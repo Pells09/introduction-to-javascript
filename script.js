baeeb2d78f79bed4d594b15e535c7a7855c7d6c4
Pells09 <105182072+Pells09@users.noreply.github.com>
2022-06-21 19:57:07 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
-document.getElementById ("name"). innerHTML = "My name is Fagbenro Pelumi.";
-document.getElementById ("height"). innerHTML = "I am 5.8ft tall.";
-document.getElementById ("country"). innerHTML = "I a from Nigeria.";
+var myName = "Fagbenro Habeeb Pelumi.";
+var height = 5.8 ;
+var country = "Nigeria." ;
+
+document.write (" My name is " + myName);
+document.write("<br>")
+document.write (" My height is " + height + " ft. ");
+document.write("<br>")
+document.write (" I am from " + country);
