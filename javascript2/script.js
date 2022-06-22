0035017c10db44e2c48c5242499d250bc323c1f7
Pells09 <105182072+Pells09@users.noreply.github.com>
2022-06-21 19:54:09 -0700

    Delete javascript2 directory

--- javascript2/script.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementById ("name"). innerHTML = "My name is Fagbenro Pelumi.";
-document.getElementById ("height"). innerHTML = "I am 5.8ft tall.";
-document.getElementById ("country"). innerHTML = "I a from Nigeria.";
